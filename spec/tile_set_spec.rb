dbcab8a44b1b0a882632f6e80b44e90d119d1937
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-17 20:02:14 -0700

    Pass returns an array of tiles of a specified length

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -10,7 +10,8 @@ describe 'A tile set' do
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      size = 10 
+      expect(TileSet.generate(size).length).to eq(10)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
